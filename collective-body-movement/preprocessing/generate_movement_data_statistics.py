dc7e2019f4724d27f0ca245365892b13c5c72f1a
jcm-art <87035357+jcm-art@users.noreply.github.com>
2023-07-19 16:50:55 -0700

    adding outline for derived statistics calculating class

---
+++ collective-body-movement/preprocessing/generate_movement_data_statistics.py
@@ -0,0 +1,16 @@
+
+
+class CollectiveBodyMovementDataStatistics:
+
+    def __init__(self) -> None:
+        # TODO - initialize input and output
+        pass
+
+    def generate_statistics_database(self):
+        # TODO - generate statistics database
+        pass
+
+if __name__ == "__main__":
+    cbmds = CollectiveBodyMovementDataStatistics()
+    cbmds.generate_statistics_database()
+    pass
