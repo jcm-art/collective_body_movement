643ca05e2541a7b883461f068388cacdf709fa4f
jcm-art <87035357+jcm-art@users.noreply.github.com>
2023-11-01 18:33:31 -0700

    Fixing init file for streamlit cloud

--- collective_body_movement/app/src/pages/__init__.py
+++ collective_body_movement/app/src/pages/__init__.py
@@ -4,7 +4,6 @@
 
 from typing import Dict, Type
 
-from .user_metrics import MetricsAnalysisPage
 from . metrics_summary import MetricsSummaryPage
 from .user_movement import MovementExplorerPage
 from ..utils import StreamlitPage
@@ -13,7 +12,6 @@ from ..utils import StreamlitPage
 CB_PAGE_MAP: Dict[str, Type[StreamlitPage]] = {
     "Metrics Summary": MetricsSummaryPage,
     "User Movement Exploration": MovementExplorerPage,
-    "User Metrics": MetricsAnalysisPage,
 }
 
 __all__ = ["CB_PAGE_MAP"]
