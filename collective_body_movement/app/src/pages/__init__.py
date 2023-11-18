27d400897359b768e5715420ea5b7adf8d924bdc
jcm-art <87035357+jcm-art@users.noreply.github.com>
2023-11-18 12:02:35 -0500

    Updating init file

--- collective_body_movement/app/src/pages/__init__.py
+++ collective_body_movement/app/src/pages/__init__.py
@@ -4,14 +4,16 @@
 
 from typing import Dict, Type
 
-from . metrics_summary import MetricsSummaryPage
+from .metrics_summary import MetricsSummaryPage
 from .user_movement import MovementExplorerPage
+from .metric_time_series import MovementExplorerPage
 from ..utils import StreamlitPage
 
 
 CB_PAGE_MAP: Dict[str, Type[StreamlitPage]] = {
     "Metrics Summary": MetricsSummaryPage,
     "User Movement Exploration": MovementExplorerPage,
+    "Metric Time Series": MetricSeriesPage,
 }
 
 __all__ = ["CB_PAGE_MAP"]
