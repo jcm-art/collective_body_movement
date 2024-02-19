Fixing bugs in algorithms (derivatives broken causing metrics to show inacurate summary results

collective_body_movement/analysis/derived_kinematics.py
@@ -3,6 +3,7 @@
 # Author: Justin Martin (jcm-art)
 
 from typing import Dict, List
+import numpy as np
 import pandas as pd
 
 from ..utils import CollectiveBodyBolt
@@ -88,11 +89,11 @@ class DerivedKinematicsBolt(CollectiveBodyBolt):
         timestep = output_metadata["fundamental_kinematics_metadata"]["avg_timestep"]
     
         # Caculate displacement for every timestep
-        output_df = output_df.assign(
-                cartesian_displacement=lambda x: (
-                    ((x["head_vel_pos_x"]*timestep)**2 + (x["head_vel_pos_y"]*timestep)**2 + (x["head_vel_pos_z"]*timestep)**2)**0.5
-                )
-            )    
+        output_df['cartesian_displacement'] = np.sqrt(
+            output_df['head_pos_x'].diff()**2 + output_df['head_pos_y'].diff()**2 + output_df['head_pos_z'].diff()**2
+        )
+        output_df['cartesian_displacement'].iloc[0] = 0  # Set distance traveled at first row as 0
+   
         
         # Calculate cumulative sum of displacement
         output_df['total_cartesian_distance'] = output_df['cartesian_displacement'].cumsum()
@@ -101,44 +102,38 @@ class DerivedKinematicsBolt(CollectiveBodyBolt):
     
     def _cumulative_rotational_distance(self, output_df: pd.DataFrame, output_metadata: Dict):
         timestep = output_metadata["fundamental_kinematics_metadata"]["avg_timestep"]
-    
+
         # Caculate displacement for every timestep
-        output_df = output_df.assign(
-                rotational_displacement=lambda x: (
-                    ((x["head_vel_rot_i"]*timestep)**2 +  \
-                      (x["head_vel_rot_j"]*timestep)**2 +  \
-                        (x["head_vel_rot_k"]*timestep)**2 + \
-                           (x["head_vel_rot_l"]*timestep)**2 \
-                            )**0.5
-                )
-            )    
+        output_df['rotational_displacement'] = np.sqrt(
+            output_df['head_rot_i'].diff()**2 + output_df['head_rot_j'].diff()**2 + output_df['head_rot_k'].diff()**2 + output_df['head_rot_l'].diff()**2
+        )
+        output_df['rotational_displacement'].iloc[0] = 0  # Set distance traveled at first row as 0
+   
         
         # Calculate cumulative sum of displacement
         output_df['total_rotational_distance'] = output_df['rotational_displacement'].cumsum()
+        
 
         return output_df, output_metadata
     
     def _linear_kinetic_energy(self, output_df: pd.DataFrame, output_metadata: Dict):
         # Caculate displacement for every timestep
-        output_df = output_df.assign(
-                linear_kinetic_energy=lambda x: (
-                    x["head_vel_pos_magnitude"]**2 * 1/2 * torso_legs_head_mass +
-                    x["left_vel_pos_magnitude"]**2 * 1/2 * hand_arm_mass +
-                    x["right_vel_pos_magnitude"]**2 * 1/2 * hand_arm_mass
-                )
-            )  
-    
+        output_df['linear_kinetic_energy'] = np.sqrt(
+            output_df['head_vel_pos_magnitude']**2 * 1/2 * torso_legs_head_mass + \
+                output_df['left_vel_pos_magnitude']**2 * 1/2 * hand_arm_mass + \
+                    output_df['right_vel_pos_magnitude']**2 * 1/2 * hand_arm_mass
+        )
+
         return output_df, output_metadata
     
     def _linear_power(self, output_df: pd.DataFrame, output_metadata: Dict):
-        # Caculate displacement for every timestep
-        output_df = output_df.assign(
-                linear_power=lambda x: (
-                    x["head_accel_pos_magnitude"]* x["head_vel_pos_magnitude"] * torso_legs_head_mass +
-                    x["left_accel_pos_magnitude"]* x["left_vel_pos_magnitude"] * hand_arm_mass +
-                    x["right_accel_pos_magnitude"]* x["right_vel_pos_magnitude"] * hand_arm_mass
-                )
-            )  
+
+        # Calculate linear power from position and velocity magnitudes        
+        output_df['linear_power'] = \
+            output_df["head_accel_pos_magnitude"]* output_df["head_vel_pos_magnitude"] * torso_legs_head_mass + \
+            output_df["left_accel_pos_magnitude"]* output_df["left_vel_pos_magnitude"] * hand_arm_mass + \
+            output_df["right_accel_pos_magnitude"]* output_df["right_vel_pos_magnitude"] * hand_arm_mass
+
     
         return output_df, output_metadata
 

collective_body_movement/analysis/fundamental_kinematics.py
@@ -155,21 +155,21 @@ class FundamentalKinematicsBolt(CollectiveBodyBolt):
         return output_df, output_metadata
     
     def _magnitude_lambda(self, output_df: pd.DataFrame, magnitude_field: str, fields: List[str]):
-        if len(fields) == 3:
-            output_df = output_df.assign(
-                magnitude_field_tmp=lambda x: (
-                    (x[fields[0]]**2 + x[fields[1]]**2 + x[fields[2]]**2)**0.5
-                )
-            )
-        elif len(fields) == 4:
-            output_df = output_df.assign(
-                magnitude_field_tmp=lambda x: (
-                    (x[fields[0]]**2 + x[fields[1]]**2 + x[fields[2]]**2 + x[fields[3]]**2)**0.5
-                )
-            ) 
 
-        # Rename temporary columns prior to returning
-        output_df.rename(columns={'magnitude_field_tmp':magnitude_field}, inplace=True)
+        # Add first three fields to dataframe
+        output_df[magnitude_field] = output_df[fields[0]]**2 + output_df[fields[1]]**2 + output_df[fields[2]]**2
+
+        if len(fields) == 4:
+            output_df[magnitude_field] += output_df[fields[3]]**2
+
+        # Check for negative values
+        if output_df[magnitude_field].min() <0:
+            print(f"The min and max of the {magnitude_field} is {output_df[magnitude_field].min()} and {output_df[magnitude_field].max()}")
+            assert False
+
+        # Take Square root to get magnitude
+        output_df[magnitude_field] = np.sqrt(output_df[magnitude_field])
+
         return output_df
 
     def _calculate_moment_arms(self, output_df: pd.DataFrame, output_metadata: Dict):
@@ -177,17 +177,17 @@ class FundamentalKinematicsBolt(CollectiveBodyBolt):
         return output_df, output_metadata
     
     def _calculate_xzplanar_moment_arm_mag(self, output_df: pd.DataFrame, output_metadata: Dict):
-        output_df = output_df.assign(
-                left_xzplanar_moment_arm_len=lambda x: (
-                    ((x["left_pos_x"]-x["head_pos_x"])**2 + (x["left_pos_z"]-x["head_pos_z"])**2)**0.5
-                )
-            )
         
-        output_df = output_df.assign(
-                right_xzplanar_moment_arm_len=lambda x: (
-                    ((x["right_pos_x"]-x["head_pos_x"])**2 + (x["right_pos_z"]-x["head_pos_z"])**2)**0.5
-                )
-            )
+        # Calculate moment arm length in xz plane
+        output_df['left_xzplanar_moment_arm_len'] = \
+            ((output_df["left_pos_x"]-output_df["head_pos_x"])**2 + \
+                (output_df["left_pos_z"]-output_df["head_pos_z"])**2)**0.5
+        
+        
+        output_df['right_xzplanar_moment_arm_len'] = \
+            ((output_df["right_pos_x"]-output_df["head_pos_x"])**2 + \
+                (output_df["right_pos_z"]-output_df["head_pos_z"])**2)**0.5
+        
         return output_df, output_metadata
 
 
@@ -198,7 +198,7 @@ class FundamentalKinematicsBolt(CollectiveBodyBolt):
             new_column_name: str, 
             timestep: float):
         
-        df[new_column_name] = df[old_column_name].diff() / timestep
+        df[new_column_name] = df[old_column_name].diff() / df['timestamp'].diff()
 
         return df
 
@@ -207,17 +207,13 @@ class FundamentalKinematicsBolt(CollectiveBodyBolt):
             output_df: pd.DataFrame, 
             new_col_name:str):
         
-        # Get data and max/min
-        # TODO (jcm-art): replace with more efficient implementation
-        data = output_df[new_col_name].to_numpy()
-        data.sort()
+        # Get get maximum value for clipping
         percentile_to_clip = 0.9
-        upper_bound_index = int(len(data)*percentile_to_clip)
-        upper_bound_val = data[upper_bound_index]
+        upper_bound_val = output_df[new_col_name].quantile(percentile_to_clip)
 
-        # Clip upper bound
-        output_df[new_col_name] = output_df[new_col_name].apply(
-            lambda x: upper_bound_val if x > upper_bound_val else x)
+        # Clip values in the 'values' column to the maximum value equal to the 90th percentile
+        output_df[new_col_name] = output_df[new_col_name].clip(upper=percentile_to_clip)
+
         
         return output_df
 